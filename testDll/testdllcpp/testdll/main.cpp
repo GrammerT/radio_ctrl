bf3f4df17c232fbc2cc9ed20032abafc9eb89384
grammer <675476193@qq.com>
2018-08-29 09:54:20 +0800

    finish testdll.

--- testDll/testdllcpp/testdll/main.cpp
+++ testDll/testdllcpp/testdll/main.cpp
@@ -11,7 +11,7 @@ typedef void*  (__stdcall *DLLCdeclInit)();
 typedef int  (__stdcall *DLLCdeclConn)(const char ** );
 typedef int  (__stdcall *DLLCdeclSetPath)(const char**,const  char** ,const char** );
 typedef int  (__stdcall *DLLCdeclStartScan)(float,float,float ,int ,const char** , float , float ,float , int  );
-typedef int  (__stdcall *DLLCdeclPhaseLockMsg)(float ,double );
+typedef int  (__stdcall *DLLCdeclPhaseLockMsg)(double ,double );
 typedef int  (__stdcall *DLLCdeclSetParam)(double ,double ,int ,int ,int ,int);
 typedef int  (__stdcall *DLLCdecladd)(int ,int );
 
@@ -29,7 +29,9 @@ int main(int argc, char *argv[])
     HMODULE hinstLib;
     //    DLLCdeclFunction DLLFunction;
 
-    hinstLib = LoadLibraryEx(L"E:\\UnizWorkspace\\QTworkspace\\radio_ctrl_new\\radio_ctrl\\dll\\debug\\ratio_dll.dll",NULL,LOAD_WITH_ALTERED_SEARCH_PATH);
+//    hinstLib = LoadLibraryEx(L"E:\\UnizWorkspace\\QTworkspace\\radio_ctrl_new\\radio_ctrl\\dll\\debug\\ratio_dll.dll",NULL,LOAD_WITH_ALTERED_SEARCH_PATH);
+    hinstLib = LoadLibraryEx(L"D:\\Opensource workspace\\radio_ctrl\\dll\\debug\\ratio_dll.dll",NULL,LOAD_WITH_ALTERED_SEARCH_PATH);
+
     DWORD dw = GetLastError();
     if (hinstLib != NULL)//成功装载动态链接库ratio_dll.dll
     {
@@ -48,9 +50,13 @@ int main(int argc, char *argv[])
         }
         int ret=0;
         DLLCdeclSetPath setpath = (DLLCdeclSetPath)GetProcAddress(hinstLib, "loadConversionAndUpConversion");
-        const char *down = "E:\\UnizWorkspace\\QTworkspace\\radio_ctrl_new\\radio_ctrl\\data\\data1\\down_conversion.txt";
-        const    char *down1 =   "E:\\UnizWorkspace\\QTworkspace\\radio_ctrl_new\\radio_ctrl\\data\\data1\\search_down_conversion.txt";
-        const char *down2 =    "E:\\UnizWorkspace\\QTworkspace\\radio_ctrl_new\\radio_ctrl\\data\\data1\\up_conversion.txt";
+//        const char *down = "E:\\UnizWorkspace\\QTworkspace\\radio_ctrl_new\\radio_ctrl\\data\\data1\\down_conversion.txt";
+//        const char *down1 =   "E:\\UnizWorkspace\\QTworkspace\\radio_ctrl_new\\radio_ctrl\\data\\data1\\search_down_conversion.txt";
+//        const char *down2 =    "E:\\UnizWorkspace\\QTworkspace\\radio_ctrl_new\\radio_ctrl\\data\\data1\\up_conversion.txt";
+        const char *down = "D:\\Opensource workspace\\radio_ctrl\\data\\data1\\down_conversion.txt";
+        const char *down1 =   "D:\\Opensource workspace\\radio_ctrl\\data\\data1\\search_down_conversion.txt";
+        const char *down2 =    "D:\\Opensource workspace\\radio_ctrl\\data\\data1\\up_conversion.txt";
+
         if(setpath!=NULL)
         {
             ret = setpath(&down,&down1,&down2);
@@ -62,7 +68,7 @@ int main(int argc, char *argv[])
             if ( DLLFunction1 != NULL)   //dll中有函数sendSetParamMsg()
             {
                 std::cout<<111111111<<std::endl;
-//                DLLFunction1(2,3,3,3,3,3);
+//                DLLFunction1(110000000,107368230,0,-30,-40,1);
                 std::cout<<222222222<<std::endl;
             }
         }
@@ -73,7 +79,7 @@ int main(int argc, char *argv[])
             if ( DLLFunction2 != NULL)   //dll中有函数sendSetParamMsg()
             {
                 std::cout<<111111111<<std::endl;
-                DLLFunction2(107368230,100,109368230,1,&down2,-10,0,-10,0);
+//                DLLFunction2(107368230,100,109368230,1,&down2,-10,0,-10,0);
                 std::cout<<222222222<<std::endl;
             }
         }
@@ -84,7 +90,7 @@ int main(int argc, char *argv[])
             if ( DLLFunction3 != NULL)   //dll中有函数sendSetParamMsg()
             {
                 std::cout<<111111111<<std::endl;
-//                DLLFunction3(2,3,3,3,3,3);
+                DLLFunction3(110000000.0,-40);
                 std::cout<<222222222<<std::endl;
             }
         }
